4b106db0a5347c38470ad400c14d2d18808f0271
RinGotou <dandeliondimn@gmail.com>
2020-03-24 19:35:00 +0800

    Improve bot message outputing

--- src/nyaabot.rb
+++ src/nyaabot.rb
@@ -1,18 +1,35 @@
+# -*- coding: UTF-8 -*-
+
 require 'telegram/bot'
 require 'toml-rb'
 require 'nokogiri'
 require 'net/http'
+# require 'accuweather'
 
 # constants
 $DEFAULT_CONFIG_FILE = './nyaabot_config.toml'
+$USER_INFO_FILE      = './nyaabot_users.toml'
 $PROXY_KEY           = 'proxy_url'
 $TOKEN_KEY           = 'token'
 
 # variables
 $bot = nil
 
+$STATIC_RESPONSE = {
+    '/start'  => '吾輩は猫である. 名前はまだ無い. ',
+    '/nyaa'   => '喵',
+    '/sqeeze' => '(类似于打雷的呼噜声)',
+    # reserved for further completion
+    '/help'  => '',
+    :none    => '这我也不太清楚, 因为我只是一只猫...'
+}
+
+def generate_msg(str)
+    puts "[#{Time.now.to_s}] #{str}"
+end
+
 def generate_error_msg(str, exception)
-    puts str
+    generate_msg(str)
     puts "Details :(#{exception.class.to_s}) #{exception.message}"
 end
 
@@ -48,23 +65,24 @@ def main_botloop
     if config == nil; return; end
     if config[:proxy_url] != nil
         Telegram::Bot::Client.run(config[:token], url:config[:proxy_url]) do |bot|
-            puts 'Listen to messages...'
+            generate_msg('Listen to messages...')
             bot.listen { |message| yield message, bot }
         end
     else
         Telegram::Bot::Client.run(config[:token]) do |bot|
-            puts 'Listen to messages...'
+            generate_msg('Listen to messages...')
             bot.listen { |message| yield message, bot }
         end
     end
 end
 
 main_botloop do |message, bot|
+    generate_msg("Receive message from #{message.chat.id}")
     case message.text
-    when '/nyaa'
-        bot.api.send_message chat_id: message.chat.id, test:'Nyaa~~~~'
+    when '/start', '/nyaa', '/help'
+        bot.api.send_message chat_id: message.chat.id, text:$STATIC_RESPONSE[message.text]
     else
-        bot.api.send_message chat_id: message.chat.id, text:'I don\'t know your request~~~~'
+        bot.api.send_message chat_id: message.chat.id, text:$STATIC_RESPONSE[:none]
     end
 end
 
